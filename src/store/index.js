fd533e0669e3ed323f2006fdede660f8201c65d5
serhiichaiuk <chayck7@gmail.com>
2022-03-31 15:25:26 +0300

    tracking and debugging state

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {
+      tours: {},
+      breakdown: [],
+      socials: {}
+    }
   },
   getters: {
   },
   mutations: {
+    setUser (state, payload) {
+      state.user = payload
+    }
   },
   actions: {
   },
